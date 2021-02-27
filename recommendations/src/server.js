e7ed2925a93887514efaa2d1b7e2e7c932c624ce
Cody Green <cody@codygreen.com>
2021-02-27 13:26:39 -0600

    fix duplicate recommendations

--- recommendations/src/server.js
+++ recommendations/src/server.js
@@ -13,10 +13,16 @@ app.use(cors());
 
 app.get('/api/recommendations', async (req, res) => {
     const keys = Object.keys(products);
-    const randomProducts=  [
-        products[parseInt(Math.random() * products.length)],
-        products[parseInt(Math.random() * products.length)],
-        products[parseInt(Math.random() * products.length)]
+    var randKeys = [];
+    while(randKeys.length < 3) {
+      var key = parseInt(Math.random() * products.length)
+      if(randKeys.indexOf(key) === -1) randKeys.push(key)
+    }
+
+    const randomProducts =  [
+        products[randKeys[0]],
+        products[randKeys[1]],
+        products[randKeys[2]]
     ]
   res.status(200).json(randomProducts);
 });

42137ef4558c15273ab6125bf289cc1749ecdae5
Eric Chen <eric.chen@f5.com>
2021-03-05 11:44:55 -0500

    add logging for inventory service

--- inventory/src/server.js
+++ inventory/src/server.js
@@ -16,6 +16,8 @@ app.get('/api/inventory', async (req, res) => {
 });
 
 app.get('/api/stats', async (req, res) => {
+    // 10.1.1.6 - - [05/Mar/2021:16:31:09 +0000] "GET /products HTTP/1.1" 200 582 "-""Envoy/HC" "-"
+    console.log(req.ip + ' - - [' + new Date().toISOString() + '] "GET /api/stats HTTP/1.1" 200 - "-" "' + req.headers['user-agent'] +'"')
   res.status(200).json({});
 })
 

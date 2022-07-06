8123c6fd3ccbc61fab6042cd0a9a253cb68e0063
Daniel Edgar <aknot242@gmail.com>
2022-07-06 14:34:46 -0700

    Add ID10T exception for lab

--- recommendations/src/server.js
+++ recommendations/src/server.js
@@ -2,26 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-const products = require('../products.json');
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/api/recommendations', async (req, res) => {
-    const keys = Object.keys(products);
-    var randKeys = [];
-    while(randKeys.length < 3) {
-      var key = parseInt(Math.random() * products.length)
-      if(randKeys.indexOf(key) === -1) randKeys.push(key)
-    }
-
-    const randomProducts =  [
-        products[randKeys[0]],
-        products[randKeys[1]],
-        products[randKeys[2]]
-    ]
-  res.status(200).json(randomProducts);
+  res.status(500).json('Unknown error: Inner exception code ID10T');
 });
 
 app.get('/api/stats', async (req, res) => {

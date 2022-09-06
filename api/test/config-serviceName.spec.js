686b049bf0ddbb932e8ab46462ead721ded49466
Daniel Edgar <aknot242@gmail.com>
2022-09-06 08:42:47 -0700

    coverage review: adding missing test cases

--- api/test/config-serviceName.spec.js
+++ api/test/config-serviceName.spec.js
@@ -2,8 +2,15 @@
 import request from 'supertest';
 import app from '../server.js';
 import { expect } from 'chai';
+import { stub } from 'sinon';
+import sinon from 'sinon';
+import LocatorService from '../services/locatorService.js'
 
 describe('GET /api/config/{serviceName}', function () {
+
+  afterEach(() => {
+    sinon.restore();
+  });
 
   it('Should return information for a configured service, "recommendations"', async function () {
     const response = await request(app)
@@ -16,15 +23,33 @@ describe('GET /api/config/{serviceName}', function () {
 
   it('Should return 404 for an unknown service', async function () {
     const response = await request(app)
-      .get('/api/config/sosuchservice')
+      .get('/api/config/nosuchservice')
       .set('Accept', 'application/json')
     expect(response.headers["content-type"]).to.match(/json/);
     expect(response.status).to.equal(404);
     expect(response.body).to.deep.equal({ error: 'service not found' });
   });
+
+  it('Should return 500 when there is an error looking up a service', async function () {
+    const locatorStub = stub(LocatorService, "getService").throws(new Error("something bad"));
+
+    const response = await request(app)
+      .get('/api/config/inventory')
+      .set('Accept', 'application/json')
+
+    expect(response.headers["content-type"]).to.match(/json/);
+    expect(response.status).to.equal(500);
+    expect(response.body).to.deep.equal({ error: 'something bad' });
+    expect(locatorStub.callCount).to.equal(1);
+  });
 });
 
 describe('POST /api/config/{serviceName}', function () {
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
   it('Should set the URL of a configured service, "inventory" to custom value', async function () {
     const response = await request(app)
       .post('/api/config/inventory')
@@ -44,9 +69,16 @@ describe('POST /api/config/{serviceName}', function () {
     expect(response.status).to.equal(200);
     expect(response.body).to.deep.equal({ name: 'inventory', url: 'http://inventory:8002' });
   });
-});
 
-describe('POST /api/config/{serviceName}', function () {
+  xit('Should throw an error when updating a service without including a URL', async function () {
+    const response = await request(app)
+      .post('/api/config/recommendations')
+      .set('Accept', 'application/json')
+    expect(response.headers["content-type"]).to.match(/json/);
+    expect(response.status).to.equal(404);
+    expect(response.body).to.deep.equal({ error: 'a service url is required' });
+  });
+
   it('Should throw an error when attempting to set the URL of an unknown service', async function () {
     const response = await request(app)
       .post('/api/config/blah')
@@ -55,5 +87,19 @@ describe('POST /api/config/{serviceName}', function () {
     expect(response.headers["content-type"]).to.match(/json/);
     expect(response.status).to.equal(404);
     expect(response.body).to.deep.equal({ error: 'service not found' });
+  });
+
+  it('Should return 500 when there is an error updating a service', async function () {
+    const locatorStub = stub(LocatorService, "setServiceUrl").throws(new Error("something bad"));
+
+    const response = await request(app)
+      .post('/api/config/inventory')
+      .set('Accept', 'application/json')
+      .send({ url: 'whatever' })
+
+    expect(response.headers["content-type"]).to.match(/json/);
+    expect(response.status).to.equal(500);
+    expect(response.body).to.deep.equal({ error: 'something bad' });
+    expect(locatorStub.callCount).to.equal(1);
   });
 });
--- api/test/stats-serviceName.spec.js
+++ api/test/stats-serviceName.spec.js
@@ -4,19 +4,26 @@ import app from '../server.js';
 import { expect } from 'chai';
 import axios from 'axios';
 import { stub } from 'sinon';
+import sinon from 'sinon';
+import { MongoClient } from 'mongodb';
 
 describe('GET /api/stats/{serviceName}', function () {
+
+  afterEach(() => {
+    sinon.restore();
+  });
 
   it('Should return an empty JSON payload when called with unknown serviceName', async function () {
     const response = await request(app)
       .get('/api/stats/blah')
       .set('Accept', 'application/json')
+
     expect(response.headers["content-type"]).to.match(/json/);
     expect(response.status).to.equal(200);
     expect(response.body).to.be.empty;
   });
 
-  it('Should return service stats when called with known serviceName', async function () {
+  it('Should return service stats when called with known serviceName: inventory', async function () {
     const axiosStub = stub(axios, "get").resolves(Promise.resolve({}));
 
     const response = await request(app)
@@ -28,5 +35,46 @@ describe('GET /api/stats/{serviceName}', function () {
     expect(response.body.host).to.equal('http://inventory:8002');
     expect(response.body.latency).to.be.a('number');
     expect(axiosStub.callCount).to.equal(1);
+  });
+
+  it('Should return service stats when called with known serviceName: database', async function () {
+    let stubFind;
+    let mockDb;
+    let mockInstanceStub;
+    let mockConnectionStub;
+
+
+    stubFind = sinon.stub().resolves({
+      project: sinon.stub().returnsThis(),
+      limit: sinon.stub().returnsThis(),
+      skip: sinon.stub().returnsThis(),
+      toArray: sinon.stub().returns([]),
+    });
+
+    mockDb = {
+      collection: () => {
+        return {
+          find: stubFind
+        };
+      }
+    };
+
+    mockInstanceStub = {
+      db: () => mockDb,
+      close: sinon.spy()
+    }
+    mockConnectionStub = stub(MongoClient, "connect").resolves(Promise.resolve(mockInstanceStub));
+
+    const response = await request(app)
+      .get('/api/stats/database')
+      .set('Accept', 'application/json')
+
+    expect(response.headers["content-type"]).to.match(/json/);
+    expect(response.status).to.equal(200);
+    expect(response.body.host).to.equal('mongodb://mongodb:27017');
+    expect(response.body.latency).to.be.a('number');
+    expect(stubFind.callCount).to.equal(1);
+    expect(mockConnectionStub.callCount).to.equal(1);
+    expect(mockInstanceStub.close.called).to.be.true;
   });
 });

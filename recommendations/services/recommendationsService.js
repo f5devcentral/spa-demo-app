ad69e1c151006268634a8c9feaade562a4e9fec4
Daniel Edgar <aknot242@gmail.com>
2022-09-19 10:46:53 -0700

    change recs service to select product file at runtime

--- recommendations/services/recommendationsService.js
+++ recommendations/services/recommendationsService.js
@@ -1,8 +1,10 @@
 import fs from 'fs/promises';
 
+const seedFile = process.env.SEED_FILE || "beerProducts.json"
+
 const recommendationsService = {
   async getRecommendations() {
-    const data = await fs.readFile('./products.json', { encoding: 'utf8' });
+    const data = await fs.readFile(`./${seedFile}`, { encoding: 'utf8' });
     const products = JSON.parse(data);
     var randKeys = [];
     while (randKeys.length < 3) {

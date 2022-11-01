d8703ffa963eb42d1f0f83664375e7f20b825eb5
Daniel Edgar <aknot242@gmail.com>
2022-11-01 09:45:37 -0400

    add coverage for error handler

---
+++ recommendations/test/server.spec.js
@@ -0,0 +1,20 @@
+
+import request from "supertest"
+import app from "../server.js"
+import { expect } from "chai"
+
+describe("Server error handler", function () {
+
+  it("Should return error when calling a service incorrectly", async function () {
+
+    const response = await request(app)
+      .post("/api/recommendations")
+      .set("Accept", "application/json")
+      .set("Content-Type", "application/json")
+      .send("{ productId: 56\" }")
+
+    expect(response.headers["content-type"]).to.match(/json/)
+    expect(response.status).to.equal(400)
+    expect(response.body).to.deep.equal({ "error": { "expose": true, "statusCode": 400, "status": 400, "body": "{ productId: 56\" }", "type": "entity.parse.failed" } })
+  })
+})

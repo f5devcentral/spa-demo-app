2f850d9a0af1d00c6dece4dc35c7156011a6e5e2
Daniel Edgar <aknot242@gmail.com>
2022-11-01 09:45:41 -0400

    Validate product exists before adding to cart

--- api/test/users-userId-cart.spec.js
+++ api/test/users-userId-cart.spec.js
@@ -9,17 +9,19 @@ import { MongoClient } from "mongodb"
 const orderProducts = [
   { "_id": "123", "id": "123", "name": "Elysian Space Dust IPA", "price": "10.49", "description": "Washington- American Double/Imperial IPA- 8.2% ABV. 73 IBUs. Pours a clear golden amber color with a thick white head. Aromas of pine and citrus with a bit of breadiness and tropical fruit. Flavors of tropical fruit, citrus and pine with notes of orange peel. Enjoy!", "imageUrl": "/images/elysian.png", "averageRating": "4.5" },
   { "_id": "234", "id": "234", "name": "New Belgium Voodoo Ranger Imperial IPA", "price": "8.99", "description": "Colorado & NC- American Double/Imperial IPA- 9% ABV. 85 IBUs. A bold imperial IPA with a rare blend of Mosaic, Calypso, Bravo, and Delta hops creating an explosion of fresh cut pine and citrus flavors.", "imageUrl": "/images/voodoo.png", "averageRating": "4.6" },
-  { "_id": "456", "id": "456", "name": "Founders KBS (Kentucky Breakfast Stout)", "price": "17.99", "description": "Michigan- American Double/Imperial Stout- 11.2% ABV. BARREL AGED. An imperial stout brewed with a massive amount of coffee and chocolates, then cave-aged in oak bourbon barrels for an entire year to make sure wonderful bourbon undertones come through in the finish. Awesome!", "imageUrl": "/images/founders.png", "averageRating": "4.6" },
 ]
-const user = {
+const aProduct = { "_id": "456", "id": "456", "name": "Founders KBS (Kentucky Breakfast Stout)", "price": "17.99", "description": "Michigan- American Double/Imperial Stout- 11.2% ABV. BARREL AGED. An imperial stout brewed with a massive amount of coffee and chocolates, then cave-aged in oak bourbon barrels for an entire year to make sure wonderful bourbon undertones come through in the finish. Awesome!", "imageUrl": "/images/founders.png", "averageRating": "4.6" }
+
+const aUser = {
   "id": "12345",
-  "cartItems": ["123", "234", "456"]
+  "cartItems": ["123", "234"]
 }
 const anUpdateResult = { "acknowledged": true, "matchedCount": 1, "modifiedCount": 1 }
 
+
 describe("GET /api/users/{userId}/cart", function () {
   let stubFind
-  let stubFindOne
+  let stubFindOneUser
   let stubUpdateOne
   let mockDb
   let mockInstanceStub
@@ -34,8 +36,8 @@ describe("GET /api/users/{userId}/cart", function () {
       toArray: sinon.stub().resolves(orderProducts),
     })
 
-    stubFindOne = sinon.stub().resolves(
-      user
+    stubFindOneUser = sinon.stub().resolves(
+      aUser
     )
 
     stubUpdateOne = sinon.stub().resolves(anUpdateResult)
@@ -44,7 +46,7 @@ describe("GET /api/users/{userId}/cart", function () {
       collection: () => {
         return {
           find: stubFind,
-          findOne: stubFindOne,
+          findOne: stubFindOneUser,
           updateOne: stubUpdateOne,
         }
       }
@@ -69,14 +71,14 @@ describe("GET /api/users/{userId}/cart", function () {
 
     expect(response.headers["content-type"]).to.match(/json/)
     expect(response.status).to.equal(200)
-    expect(response.body.length).to.equal(3)
+    expect(response.body.length).to.equal(2)
     expect(stubFind.callCount).to.equal(1)
     expect(mockConnectionStub.callCount).to.equal(1)
     expect(mockInstanceStub.close.called).to.be.true
   })
 
   it("Should emit a 404 error if the user cart cannot be found", async function () {
-    stubFindOne = sinon.stub().resolves(
+    stubFindOneUser = sinon.stub().resolves(
       null
     )
     mockConnectionStub = stub(MongoClient, "connect").resolves(Promise.resolve(mockInstanceStub))
@@ -106,8 +108,10 @@ describe("GET /api/users/{userId}/cart", function () {
 })
 
 describe("POST /api/users/{userId}/cart", function () {
+
   let stubFind
-  let stubFindOne
+  let stubFindOneProduct
+  let stubFindOneUser
   let stubUpdateOne
   let mockDb
   let mockInstanceStub
@@ -122,18 +126,33 @@ describe("POST /api/users/{userId}/cart", function () {
       toArray: sinon.stub().resolves(orderProducts),
     })
 
-    stubFindOne = sinon.stub().resolves(
-      user
+    stubFindOneProduct = sinon.stub().resolves(
+      aProduct
+    )
+
+    stubFindOneUser = sinon.stub().resolves(
+      aUser
     )
 
     stubUpdateOne = sinon.stub().resolves(anUpdateResult)
 
     mockDb = {
-      collection: () => {
-        return {
-          find: stubFind,
-          findOne: stubFindOne,
-          updateOne: stubUpdateOne,
+      collection: (collectionType) => {
+        switch (collectionType) {
+          case "products":
+            return {
+              find: stubFind,
+              findOne: stubFindOneProduct,
+              updateOne: stubUpdateOne,
+            }
+          case "users":
+            return {
+              find: stubFind,
+              findOne: stubFindOneUser,
+              updateOne: stubUpdateOne,
+            }
+          default:
+            return {}
         }
       }
     }
@@ -158,28 +177,50 @@ describe("POST /api/users/{userId}/cart", function () {
 
     expect(response.headers["content-type"]).to.match(/json/)
     expect(response.status).to.equal(200)
-    expect(response.body.length).to.equal(3)
+    expect(response.body.length).to.equal(2)
+    expect(stubFindOneProduct.callCount).to.equal(1)
+    expect(stubFindOneUser.callCount).to.equal(1)
     expect(stubFind.callCount).to.equal(1)
     expect(stubUpdateOne.callCount).to.equal(1)
-    expect(mockConnectionStub.callCount).to.equal(1)
+    expect(mockConnectionStub.callCount).to.equal(2)
     expect(mockInstanceStub.close.called).to.be.true
   })
 
   it("Should emit a 404 error if the user does not exist in the database", async function () {
-    stubFindOne = sinon.stub().resolves(null)
+    stubFindOneUser = sinon.stub().resolves(null)
     mockConnectionStub = stub(MongoClient, "connect").resolves(Promise.resolve(mockInstanceStub))
 
     const response = await request(app)
       .post("/api/users/99999/cart")
       .set("Accept", "application/json")
-      .send({ productId: "112" })
+      .send({ productId: "456" })
 
     expect(response.headers["content-type"]).to.match(/json/)
     expect(response.status).to.equal(404)
     expect(response.body).to.deep.equal({ error: "Could not find the user!" })
-    expect(stubFindOne.callCount).to.equal(1)
+    expect(stubFindOneProduct.callCount).to.equal(1)
+    expect(stubFindOneUser.callCount).to.equal(1)
     expect(stubUpdateOne.callCount).to.equal(1)
-    expect(mockConnectionStub.callCount).to.equal(1)
+    expect(mockConnectionStub.callCount).to.equal(2)
+    expect(mockInstanceStub.close.called).to.be.true
+  })
+
+  it("Should emit a 404 error if the product being added does not exist in the database", async function () {
+    stubFindOneProduct = sinon.stub().resolves(null)
+    mockConnectionStub = stub(MongoClient, "connect").resolves(Promise.resolve(mockInstanceStub))
+
+    const response = await request(app)
+      .post("/api/users/99999/cart")
+      .set("Accept", "application/json")
+      .send({ productId: "456" })
+
+    expect(response.headers["content-type"]).to.match(/json/)
+    expect(response.status).to.equal(404)
+    expect(response.body).to.deep.equal({ error: "Could not find the product!" })
+    expect(stubFindOneProduct.callCount).to.equal(1)
+    expect(stubFindOneUser.callCount).to.equal(0)
+    expect(stubUpdateOne.callCount).to.equal(0)
+    expect(mockConnectionStub.callCount).to.equal(2)
     expect(mockInstanceStub.close.called).to.be.true
   })
 
